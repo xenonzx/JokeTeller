1df48ecde6cdda72c9f968e99557f06ff67c20a0
Ahmed El-Dakhakhny <ahmed0xenon@gmail.com>
2016-09-06 06:48:46 +0200

    adding string array of jokes

--- jokesproviderlib/src/main/java/com/example/JokesProvider.java
+++ jokesproviderlib/src/main/java/com/example/JokesProvider.java
@@ -1,4 +1,25 @@
 package com.example;
 
 public class JokesProvider {
+    String[]Jokes={
+            "Q: what`s the object oriented way to get wealthy? " +
+                    "\n A: Inheritance "
+            ,
+            "Q: Why do programmers always mix up Halloween and Christmas?" +
+                    "\n" +
+                    "A: Because Oct 31 == Dec 25!"
+            ,"“Knock, knock.”\n" +
+            "\n" +
+            "“Who’s there?”\n" +
+            "\n" +
+            "very long pause….\n" +
+            "\n" +
+            "“Java.”\n" +
+            "\n" +
+            ":-o\n" +
+            "\n"
+
+    };
+
+
 }
