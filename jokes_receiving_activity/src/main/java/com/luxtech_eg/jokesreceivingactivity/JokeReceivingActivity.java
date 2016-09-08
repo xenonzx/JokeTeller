741a63f6fcc5e74c3b96b4728fedaed25820c874
Ahmed El-Dakhakhny <ahmed0xenon@gmail.com>
2016-09-08 07:02:21 +0200

    adding activity

---
+++ jokes_receiving_activity/src/main/java/com/luxtech_eg/jokesreceivingactivity/JokeReceivingActivity.java
@@ -0,0 +1,13 @@
+package com.luxtech_eg.jokesreceivingactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JokeReceivingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke_receiving);
+    }
+}
