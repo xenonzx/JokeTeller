7f85dfdff162be8de8901e4472b65a2989b7a1cb
Ahmed El-Dakhakhny <ahmed0xenon@gmail.com>
2016-09-26 10:11:55 +0200

    loading add in adview

--- app/src/main/java/com/luxtech_eg/joketellerfinalproject/MainActivity.java
+++ app/src/main/java/com/luxtech_eg/joketellerfinalproject/MainActivity.java
@@ -3,20 +3,26 @@ package com.luxtech_eg.joketellerfinalproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.luxtech_eg.jokesreceivingactivity.JokeReceivingActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity  implements JokeReceivedListener{
+    private static final String TAG = MainActivity.class.getSimpleName();
     @BindView(R.id.b_tell_jokes)
     Button jokesButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-6991334778086285~5079986161");
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         jokesButton.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +33,14 @@ public class MainActivity extends AppCompatActivity  implements JokeReceivedList
             }
         });
 
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        if(mAdView!=null){
+            Log.v(TAG,"mAdView exists   ");
+            mAdView.loadAd(adRequest);
+        } else{
+            Log.v(TAG,"mAdView is null");
+        }
     }
 
     @Override
